c38e3d83ed819de30bd8dba76b7e031b95f72a23
Данияр Тыныбеков <donnietruth@gmail.com>
2019-02-18 11:16:08 +0600

    added new tests for digits and words with marks

--- tests.py
+++ tests.py
@@ -1,23 +1,29 @@
 from bs4 import BeautifulSoup
 import unittest
 import requests
+import re
 
 
 class ChangeHabraPagesTest(unittest.TestCase):
 
+    def setUp(self):
+        self.url = "http://localhost:8003/ru/"
+
     def test_right_change_words_on_page(self):
-        article_page_url = "http://localhost:8003/ru/post/439856/"
-        response = requests.get(article_page_url)
+        response = requests.get("{}post/439856/".format(self.url))
         soup = BeautifulSoup(response.content, 'lxml')
         assert soup.find('span', {'class': 'post__title-text'}).text.strip() == "Яндекс™! Спасибо за Uber"
         assert soup.find('a', {'class': 'user-info__fullname'}).text.strip() == "Rustam™ Sultansoy"
 
     def test_should_return_page_with_digits_without_changes(self):
-        page_url = "http://localhost:8003/ru/"
-        response = requests.get(page_url)
+        response = requests.get(self.url)
         soup = BeautifulSoup(response.content, "lxml")
-        assert soup.find("span", {"class": "companies-rating__rating"}).text.strip() == "1743,00"
+        assert len(soup.find("span", {"class": "companies-rating__rating"}).text.strip()) == 7
 
+    def test_should_change_word_with_marks_between_letters(self):
+        response = requests.get("{}post/218855/".format(self.url))
+        soup = BeautifulSoup(response.content, 'lxml')
+        assert "Erlang™/OTP" in soup.find("span", {"class": "post__title-text"}).text
 
 if __name__ == "__main__":
     unittest.main()
